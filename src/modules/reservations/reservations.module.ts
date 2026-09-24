659d1888a1c2707c09f7fbca2f94fd350a938639
luis-suarez <luismasuarezsevilla@gmail.com>
2026-09-24 16:54:57 -0500

    fix(reservations): registrar repositorios de Table y Customer en el módulo

--- src/modules/reservations/reservations.module.ts
+++ src/modules/reservations/reservations.module.ts
@@ -3,9 +3,11 @@ import { ReservationsService } from './reservations.service.js';
 import { ReservationsController } from './reservations.controller.js';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Reservation } from './entities/reservation.entity.js';
+import { Table } from '../tables/entities/table.entity.js';
+import { Customer } from '../customers/entities/customer.entity.js';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reservation])],
+  imports: [TypeOrmModule.forFeature([Reservation, Table, Customer])],
   controllers: [ReservationsController],
   providers: [ReservationsService],
 })
