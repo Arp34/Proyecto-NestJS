ae0492c1208cfc1adbc2ac84eb7b815653addc94
doctorpatitas <diegogonzal272111@gmail.com>
2026-09-25 15:33:38 -0500

    fix: se ha eliminado la posibilidad de mandaar nombres con espacios

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -48,6 +48,20 @@ export class ProductService {
 
   async update(id: string, updateProductDto: UpdateProductDto) {
     const productUpdate = await this.findOne(id);
+
+    if(updateProductDto.name !== undefined){
+      updateProductDto.name = updateProductDto.name.trim();
+    }
+
+    if(updateProductDto.description !== undefined){
+      updateProductDto.description = updateProductDto.description.trim();
+    }
+
+    if(updateProductDto.imageUrl !== undefined){
+      updateProductDto.imageUrl = updateProductDto.imageUrl.trim();
+    }
+
+
     this.productRepository.merge(productUpdate, updateProductDto);
 
     return await this.productRepository.save(productUpdate);
