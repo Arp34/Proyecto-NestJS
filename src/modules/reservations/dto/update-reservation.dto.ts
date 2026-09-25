d66b3283a3835bcd608661908ccc33452b72294f
luis-suarez <luismasuarezsevilla@gmail.com>
2026-09-25 17:35:24 -0500

    fix: enum y arreglo del update

--- src/modules/reservations/dto/update-reservation.dto.ts
+++ src/modules/reservations/dto/update-reservation.dto.ts
@@ -1,9 +1,15 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateReservationDto } from './create-reservation.dto.js';
 import { IsEnum, IsOptional } from 'class-validator';
 import { ReservationStatus } from '../entities/reservation.entity.js';
 
 export class UpdateReservationDto extends PartialType(CreateReservationDto) {
+    @ApiPropertyOptional({
+        enum:ReservationStatus,
+        description: 'Estado de la reserva',
+        example: ReservationStatus.PENDING
+    })
+    
     @IsOptional()
     @IsEnum(ReservationStatus)
     status?: ReservationStatus;
